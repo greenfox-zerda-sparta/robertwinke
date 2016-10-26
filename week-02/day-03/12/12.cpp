9b85471bb481f9b4ade73ca440e96ca5ce62943e
Robert Winke <robertwinke@gmail.com>
2016-10-26 21:05:58 +0200

    size of result array improved

--- week-02/day-03/12/12.cpp
+++ week-02/day-03/12/12.cpp
@@ -34,12 +34,27 @@ void safeUnion(int *a, int l_a, int *b, int l_b, int *result, int l_res) {
   }
 }
 
+int numberOfRepeats(int *a, int l_a, int *b, int l_b){
+  int counter = 0;
+  for (int i = 0; i < l_a; i++){
+    for (int j = 0; j < l_b; j++){
+      if(a[i] == b[j]){
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
+
 int main(int argc, char** argv) {
   int a[3] = {1,2,3};
-  int b[3] = {4,3,6};
-  int c[6];
-  safeUnion(a,3,b,3,c,6);
-  for (int i = 0; i<6; i++) cout <<c[i] << endl;
+  int b[3] = {4,3,1};
+
+  int number = 6 - numberOfRepeats(a,3,b,3);
+  int c[number];
+
+  safeUnion(a,3,b,3,c,number);
+  for (int i = 0; i<number; i++) cout <<c[i] << endl;
 
 }
 
