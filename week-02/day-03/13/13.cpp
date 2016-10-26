0ae35db0d9e5d47326b4c69b21b65850ebd39bf2
Robert Winke <robertwinke@gmail.com>
2016-10-26 17:17:50 +0200

    13 partial solution

---
+++ week-02/day-03/13/13.cpp
@@ -0,0 +1,46 @@
+/*
+ * 13.cpp
+ *
+ *  Created on: Oct 26, 2016
+ *      Author: robertwinke
+ */
+/*
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Create a function that prints each word in a string into a new line!
+// Create an other function that takes a string and an array of strings as it's input
+// and puts each word in the original string into a new element in the array of strings.
+// Decide which argument should be a pointer and which shouldn't.
+// In the main function, print out each element of the array into a new line!
+
+
+void printString(string sentence){
+  for(int i = 0; sentence[i] != '\0'; i++) {
+    if (sentence[i] != ' '){
+      cout << sentence[i];
+    }
+    else {
+      cout << endl;
+    }
+  }
+}
+
+void stringAdder(string sentence, string *array, int l_array){
+
+
+}
+
+int main(int argc, char **argv){
+  string sentence = "dssds sdsda sdfds";
+  string array[5] = {"fsgdfgd", "ddfgdd"};
+
+  printString(sentence);
+
+  return 0;
+}
+
+*/
+
