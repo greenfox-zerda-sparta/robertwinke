0b0b77675848e7bd6b3640593d52fafbe66847de
Robert Winke <robertwinke@gmail.com>
2016-10-27 18:00:14 +0200

    10 not working

---
+++ week-02/day-04/10/10.cpp
@@ -0,0 +1,63 @@
+/*
+ * 10.cpp
+ *
+ *  Created on: Oct 27, 2016
+ *      Author: robertwinke
+ */
+
+
+
+#include <iostream>
+#include <string>
+
+// Create a game that figures a number that the user picked between 0 and 10
+// It should ask questions like:
+//  - Is the number greater than 5? [y/n]
+//  - Is the number smaller than 4? [y/n]
+// (don't have to ask both kind)
+//
+// It should wait for input after the question
+//
+// If the answer is not "y" or "n" it should re-ask the question;
+//
+// If the program knows enough it should tell the picked number like:
+// Your number is: 8
+
+using namespace std;
+
+int main() {
+  int min;
+  int max;
+  char input;
+
+  cout << "set a min and a max" << endl;
+  cin >> min >> max;
+
+  cout << "is it smaller than " << max << "[y/n]" <<endl;
+  cin >> input;
+  if (input == 'n'){
+    cout << "your answer is " << max << endl;
+  }
+  else {
+    while (min != max){
+      if (input == 'y'){
+        min = (min + max) / 2;
+        cout << "is it smaller than " << min << endl;
+        cin >> input;
+      }
+      else if (input == 'n') {
+        max = (min + max) / 2;
+        cout << "is it smaller than " << max << endl;
+        cin >> input;
+      }
+      else {
+        cout << "wrong input try again [y/n]" << endl;
+        cin >> input;
+      }
+    }
+  }
+
+  cout << "you thoguht of "<< max << endl;
+  return 0;
+}
+
