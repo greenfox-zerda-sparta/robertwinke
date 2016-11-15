44966029ff9f427c2e5bdf3912bd92578933ec8a
Robert Winke <robertwinke@gmail.com>
2016-11-15 17:08:37 +0100

    outsourcing to function

--- week-05/day-02/12/12.cpp
+++ week-05/day-02/12/12.cpp
@@ -6,19 +6,15 @@
  */
 
 
-
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-int main() {
-
+void encryption (int offset) {
   ifstream my_file("twelfth-exercise.txt");
   ofstream output_file("twelfth-output.txt");
   string line;
-
-  int offset = 1;
 
   if (my_file.is_open()) {
     while(getline(my_file,line)) {
@@ -33,14 +29,15 @@ int main() {
     }
     output_file << "\n";
   }
-
-
-
-
   my_file.close();
   output_file.close();
 
 
+}
+
+int main() {
+
+  encryption(1);
   return 0;
 }
 
